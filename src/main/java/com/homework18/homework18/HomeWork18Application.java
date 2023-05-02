cab9d1a1b56c446db9f6535055c4464237acbc2f
Limo <christmasboom@mail.ru>
2023-05-02 13:07:25 +0300

    Создание ветки

--- src/main/java/com/homework18/homework18/HomeWork18Application.java
+++ src/main/java/com/homework18/homework18/HomeWork18Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomeWork18Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HomeWork18Application.class, args);
 	}
 
